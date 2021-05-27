300857e184eedbca1f3431bb052eb82e6f29b68f
Daniel Mueller <danielpetermueller@gmail.com>
2021-05-27 09:13:35 -0400

    commit tree add321c1aa64d55cbee036642384f0673c4401c0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+  //querySelectors
+let dogTable = document.querySelector('#dog-table')
+let dogForm = document.querySelector('#dog-form')
+// Fetch Requests
 
-})
+fetch('http://localhost:3000/dogs')
+.then(resp => resp.json())
+.then(data => appendDogDataToTable(data))
+
+//Manipulate Data
+function appendDogDataToTable(dogs){
+    dogs.forEach(function(dog){
+        let tr = document.createElement("tr");   
+        tr.innerHTML =
+       ` <tr>
+            <td>${dog.name}</td>
+            <td>${dog.breed}<td>
+            <td>${dog.sex}</td>
+            <td><button data-id=${dog.id}>Edit</button></td>
+        </tr>`
+        dogTable.append(tr);
+    })
+    
+}
